e66b0b3092a108f6556669dc3f81c0d8ed4018e9
jamesmcc90 <james.mcc90@gmail.com>
2018-11-22 19:41:16 +0000

    Error handling for no internet

---
+++ app/src/main/java/com/example/james/testapp/VerseNotificationActivity.java
@@ -0,0 +1,26 @@
+package com.example.james.testapp;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerseNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verse_notification);
+
+
+        NotificationManager notification =(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notify = new Notification.Builder
+                (getApplicationContext()).setContentTitle("Verse of the Day").setContentText("").build();
+
+        notify.flags |= Notification.FLAG_AUTO_CANCEL;
+        notification.notify(0, notify);
+
+    }
+
+}
