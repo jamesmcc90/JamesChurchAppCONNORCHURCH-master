5852dcd0613a481c7b7d76a0945dad4d32b4a886
jamesmcc90 <james.mcc90@gmail.com>
2019-05-26 09:43:35 +0100

    Added information/image to Men organisations. Changed text colour/item background to rotas. Added image to Scroll View in About Us. Minor UI adjustments to fragments.

--- app/src/main/java/com/connorchurch/james/churchapp/AboutUsActivity.java
+++ app/src/main/java/com/connorchurch/james/churchapp/AboutUsActivity.java
@@ -32,6 +32,7 @@ public class AboutUsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+
     }
 
 
--- app/src/main/java/com/connorchurch/james/churchapp/RotasActivity.java
+++ app/src/main/java/com/connorchurch/james/churchapp/RotasActivity.java
@@ -37,10 +37,10 @@ public class RotasActivity extends AppCompatActivity{
             categories.add("Finance");
 
             // Creating adapter for spinner
-            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, categories);
 
             // Drop down layout style - list view with radio button
-            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter.setDropDownViewResource(R.layout.spinner_item);
 
             // attaching data adapter to spinner
             spinner.setAdapter(dataAdapter);
