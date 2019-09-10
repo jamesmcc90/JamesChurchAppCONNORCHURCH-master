4e762a8b38ccdb32c178006809d9af3beb67880a
jamesmcc90 <james.mcc90@gmail.com>
2019-09-10 20:46:24 +0100

    Updated profile info for Minister

--- app/src/main/java/com/connorchurch/james/churchapp/Minister.java
+++ app/src/main/java/com/connorchurch/james/churchapp/Minister.java
@@ -6,6 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+
+import com.connorchurch.james.churchapp.activities.AudioStreamActivity;
+import com.connorchurch.james.churchapp.activities.MainActivity;
 
 public class Minister extends AppCompatActivity {
 
@@ -15,6 +19,14 @@ public class Minister extends AppCompatActivity {
         setContentView(R.layout.activity_minister);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public void onBackPressed(){
+
+        Intent first = new Intent(Minister.this, MainActivity.class);
+        startActivity(first);
 
     }
 }
